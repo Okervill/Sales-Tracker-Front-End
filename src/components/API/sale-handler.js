68b113d4903ccd156069b4b76259e0e30438b9ff
Patrick <Pat@lardner.eu>
2021-05-31 03:30:49 +0100

    ssl set up

--- src/components/API/sale-handler.js
+++ src/components/API/sale-handler.js
@@ -5,7 +5,7 @@ export const postsale = (image) => {
         console.log('post submitted')
         let form_data = new FormData();
         form_data.append('receiptdata', image)
-        let url = 'http://api.albayan.io/sale/post';
+        let url = 'https://api.albayan.io/sale/post';
         axios.post(url, form_data, {
             headers: {
                 'content-type': 'multipart/form-data'
