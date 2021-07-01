4a5293699028305bdb291eff2a6ddf08761ffedc
Patrick Lardner <16062927+Okervill@users.noreply.github.com>
2021-07-02 00:08:03 +0100

    Front end work on targets

--- src/components/ManagerDash/index.js
+++ src/components/ManagerDash/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { compose } from 'recompose';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import { withFirebase } from '../Firebase';
 import { withAuthorisation } from '../Session';
-import * as ROLES from '../../constants/roles';
+import * as ROLES from '../../constants/roles'
+
+import './managers.css'
 
 import { getRateCards, disableUser, createUser, postRateCard, activateRatecard } from '../API/sale-handler'
 
@@ -23,7 +26,18 @@ const INITIALSTATE = {
     updatingRatecard: undefined,
     updatingRateCardButtonDisabled: true,
     uploadedRatecard: undefined,
-    uploadedRatecardName: null
+    uploadedRatecardName: null,
+    target_new: 0,
+    target_upg: 0,
+    target_payg: 0,
+    target_hbbnew: 0,
+    target_ciot: 0,
+    target_tech: 0,
+    target_bus: 0,
+    target_rev: 0,
+    totalhours: 0,
+    hours: {},
+    weightings: {}
 }
 
 class AdminPage extends Component {
@@ -61,7 +75,11 @@ class AdminPage extends Component {
 
         getUsers(this.props.firebase, this.state.selectedStore)
             .then(userList => {
-                this.setState({ users: userList });
+                let weightings = {}
+                for (let user of userList) {
+                    weightings[user.uid] = 100
+                }
+                this.setState({ users: userList, weightings });
             })
             .catch(err => {
                 console.error(err);
@@ -126,6 +144,11 @@ class AdminPage extends Component {
             .then(token => {
                 loadRateCards(token, this.state.selectedStore)
                     .then(ratecards => {
+                        for (let ratecard of ratecards) {
+                            if (ratecard.active === 1) {
+                                this.setState({ activeRatecard: ratecard })
+                            }
+                        }
                         this.setState({ ratecards })
                     })
                     .catch(err => {
@@ -261,75 +284,194 @@ class AdminPage extends Component {
             })
     }
 
+    onTargetsChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    onHoursChange = event => {
+        let hours = this.state.hours;
+        hours[event.target.name] = event.target.value
+        let totalhours = calculateTotalHours(hours, this.state.weightings);
+        this.setState({ hours, totalhours });
+    }
+
+    onWeightingChange = event => {
+        let weightings = this.state.weightings;
+        weightings[event.target.name] = event.target.value;
+        let totalhours = calculateTotalHours(this.state.hours, weightings);
+        this.setState({ weightings, totalhours });
+    }
+
     render() {
 
         const { firstname, surname, email, password, formerror, formsuccess, newUserButtonDisabled,
-            selectedStore, stores, users, ratecards, activeRatecard, updatingRatecard, uploadedRatecardName } = this.state;
+            selectedStore, stores, users, ratecards, activeRatecard, updatingRatecard, uploadedRatecardName,
+            target_new, target_upg, target_payg, target_hbbnew, target_bus, target_ciot, target_rev, target_tech } = this.state;
 
         return (
             <div>
                 <h1>Admin</h1>
-                {stores.map(store => (
-                    <button name={store.storecode} onClick={this.updateStore}>{store.storecode}</button>
-                ))}
 
+
+                {stores.map(store => (
+                    <>
+                        <button name={store.storecode} onClick={this.updateStore}>{store.storecode}</button>
+                    </>
+                ))}
                 <h2>Selected Store: {selectedStore}</h2> <br />
 
                 {formsuccess}
 
-                <h3>New User</h3>
-                <form>
-                    <label htmlFor='firstname'>First Name </label>
-                    <input type='text' name='firstname' placeholder='Firstname' value={firstname} onChange={this.onChange} /><br />
-                    <label htmlFor='surname'>Last Name </label>
-                    <input type='text' name='surname' placeholder='Surname' value={surname} onChange={this.onChange} /><br />
-                    <label htmlFor='email'>Email Address </label>
-                    <input type='text' name='email' placeholder='Email Address' value={email} onChange={this.onChange} /><br />
-                    <label htmlFor='password'>Password </label>
-                    <input type='password' name='password' placeholder='Password' value={password} onChange={this.onChange} /><br />
-                </form>
-                <button type='submit' name='saveuser' onClick={this.saveUserHandler} disabled={newUserButtonDisabled}>Save</button>
-                {formerror}
+                <Tabs>
+                    <TabList>
+                        <Tab>Staff</Tab>
+                        <Tab>Targets</Tab>
+                        <Tab>Ratecards</Tab>
+                    </TabList>
+                    <TabPanel>
+                        <div className="admin-newuser-container">
+                            <h3 className="admin-newuser-form-title">New User</h3>
+                            <form className="admin-newuser-form">
+                                <label htmlFor='firstname'>First Name </label>
+                                <input type='text' name='firstname' placeholder='Firstname' value={firstname} onChange={this.onChange} /><br />
+                                <label htmlFor='surname'>Last Name </label>
+                                <input type='text' name='surname' placeholder='Surname' value={surname} onChange={this.onChange} /><br />
+                                <label htmlFor='email'>Email Address </label>
+                                <input type='text' name='email' placeholder='Email Address' value={email} onChange={this.onChange} /><br />
+                                <label htmlFor='password'>Password </label>
+                                <input type='password' name='password' placeholder='Password' value={password} onChange={this.onChange} /><br />
+                            </form>
+                            <button className="admin-newuser-submit" type='submit' name='saveuser' onClick={this.saveUserHandler} disabled={newUserButtonDisabled}>Save</button>
+                            {formerror}
+                        </div>
 
+                        <br /><br /><br />
 
-                <br /><br /><br />
+                        <h3>All Users</h3>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Disable User</th>
+                                </tr>
+                            </thead>
+                            {users && users.length > 0 ? users.map(user => (
+                                <tbody>
+                                    <tr>
+                                        <td>{user.firstname} {user.surname}</td>
+                                        <td>{user.email}</td>
+                                        <td><button name={user.uid} onClick={this.disableUser}>{user.disabled ? 'Activate' : 'Disable'}</button></td>
+                                    </tr>
+                                </tbody>
+                            )) : <tbody><tr><td>No staff found.</td></tr></tbody>}
+                        </table>
+                    </TabPanel>
+                    <TabPanel>
+                        <h3>Store Targets - Under construction</h3>
+                        <table className='targets-table'>
+                            <thead className='targets-table-head'>
+                                <tr>
+                                    <th>New</th>
+                                    <th>Upg</th>
+                                    <th>PAYG</th>
+                                    <th>HBB</th>
+                                    <th>CIOT</th>
+                                    <th>Tech</th>
+                                    <th>Business</th>
+                                    <th>Rev</th>
+                                </tr>
+                            </thead>
+                            <tbody className='targets-table-body'>
+                                <tr>
+                                    <td><input className='targets-table-input' name='target_new' value={target_new} onChange={this.onTargetsChange} /></td>
+                                    <td><input className='targets-table-input' name='target_upg' value={target_upg} onChange={this.onTargetsChange} /></td>
+                                    <td><input className='targets-table-input' name='target_payg' value={target_payg} onChange={this.onTargetsChange} /></td>
+                                    <td><input className='targets-table-input' name='target_hbbnew' value={target_hbbnew} onChange={this.onTargetsChange} /></td>
+                                    <td><input className='targets-table-input' name='target_ciot' value={target_ciot} onChange={this.onTargetsChange} /></td>
+                                    <td><input className='targets-table-input' name='target_tech' value={target_tech} onChange={this.onTargetsChange} /></td>
+                                    <td><input className='targets-table-input' name='target_bus' value={target_bus} onChange={this.onTargetsChange} /></td>
+                                    <td><input className='targets-table-input' name='target_rev' value={target_rev} onChange={this.onTargetsChange} /></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table className='weightings-table'>
+                            <thead>
+                                <tr>
+                                    <th>Staff</th>
+                                    <th>Hours Worked</th>
+                                    <th>Weighting</th>
+                                    <th>New</th>
+                                    <th>Upg</th>
+                                    <th>PAYG</th>
+                                    <th>HBB</th>
+                                    <th>CIOT</th>
+                                    <th>Tech</th>
+                                    <th>Business</th>
+                                    <th>Rev</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {users.map(user => (
+                                    <tr>
+                                        <td>{user.firstname} {user.surname}</td>
+                                        <td><input name={user.uid} className='targets-table-input' value={this.state.hours.uid} onChange={this.onHoursChange} /></td>
+                                        <td>
+                                            <select name={user.uid} value={this.state.weightings.uid} onChange={this.onWeightingChange}>
+                                                <option>100</option>
+                                                <option>95</option>
+                                                <option>90</option>
+                                                <option>85</option>
+                                                <option>80</option>
+                                                <option>75</option>
+                                                <option>70</option>
+                                                <option>65</option>
+                                                <option>60</option>
+                                                <option>55</option>
+                                                <option>50</option>
+                                                <option>45</option>
+                                                <option>40</option>
+                                                <option>35</option>
+                                                <option>30</option>
+                                                <option>25</option>
+                                                <option>20</option>
+                                                <option>15</option>
+                                                <option>10</option>
+                                                <option>5</option>
+                                                <option>0</option>
+                                            </select>
+                                        </td>
+                                        <td><label>{Math.ceil(this.state.target_new / this.state.totalhours * this.state.hours[user.uid] * (this.state.weightings[user.uid] / 100))}</label></td>
+                                        <td><label>{Math.ceil(this.state.target_upg / this.state.totalhours * this.state.hours[user.uid] * (this.state.weightings[user.uid] / 100))}</label></td>
+                                        <td><label>{Math.ceil(this.state.target_payg / this.state.totalhours * this.state.hours[user.uid] * (this.state.weightings[user.uid] / 100))}</label></td>
+                                        <td><label>{Math.ceil(this.state.target_hbbnew / this.state.totalhours * this.state.hours[user.uid] * (this.state.weightings[user.uid] / 100))}</label></td>
+                                        <td><label>{Math.ceil(this.state.target_ciot / this.state.totalhours * this.state.hours[user.uid] * (this.state.weightings[user.uid] / 100))}</label></td>
+                                        <td><label>{Math.ceil(this.state.target_tech / this.state.totalhours * this.state.hours[user.uid] * (this.state.weightings[user.uid] / 100))}</label></td>
+                                        <td><label>{Math.ceil(this.state.target_bus / this.state.totalhours * this.state.hours[user.uid] * (this.state.weightings[user.uid] / 100))}</label></td>
+                                        <td><label>{Math.ceil(this.state.target_rev / this.state.totalhours * this.state.hours[user.uid] * (this.state.weightings[user.uid] / 100))}</label></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </TabPanel>
+                    <TabPanel>
+                        <h3>New Ratecard</h3>
+                        <input type="file" name="uploadedRatecard" onChange={this.onChange} /><br />
+                        <input type="text" name="uploadedRatecardName" placeholder='Ratecard name' value={uploadedRatecardName} onChange={this.onChange} />
+                        <button onClick={this.uploadRateCard}>Submit</button><br />
 
-                <h3>All Users</h3>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Disable User</th>
-                        </tr>
-                    </thead>
-                    {users && users.length > 0 ? users.map(user => (
-                        <tbody>
-                            <tr>
-                                <td>{user.firstname} {user.surname}</td>
-                                <td>{user.email}</td>
-                                <td><button name={user.uid} onClick={this.disableUser}>{user.disabled ? 'Activate' : 'Disable'}</button></td>
-                            </tr>
-                        </tbody>
-                    )) : <tbody><tr><td>No staff found.</td></tr></tbody>}
-                </table>
-
-                <br /><br /><br />
-                <h3>New Ratecard</h3>
-                <input type="file" name="uploadedRatecard" onChange={this.onChange} /><br />
-                <input type="text" name="uploadedRatecardName" placeholder='Ratecard name' value={uploadedRatecardName} onChange={this.onChange} />
-                <button onClick={this.uploadRateCard}>Submit</button><br />
-
-                <br /><br /><br />
-                <h3>All Ratecards</h3>
-                <p>Active card: {activeRatecard?.tablename.substr(5)}</p>
-                <p>Change ratecard?</p>
-                <select name='updatingRatecard' value={updatingRatecard} onChange={this.onChange}>
-                    {ratecards && ratecards.length > 0 ? ratecards.map(ratecard => (
-                        <option>{ratecard.tablename.substr(5)}</option>
-                    )) : <option>No rate cards found.</option>}
-                </select>
-                <button name='update-ratecard' onClick={this.updateRateCard}>Save</button>
+                        <br /><br /><br />
+                        <h3>All Ratecards</h3>
+                        <p>Active card: {activeRatecard?.tablename.substr(5)}</p>
+                        <p>Change ratecard?</p>
+                        <select name='updatingRatecard' value={updatingRatecard} onChange={this.onChange}>
+                            {ratecards && ratecards.length > 0 ? ratecards.map(ratecard => (
+                                <option>{ratecard.tablename.substr(5)}</option>
+                            )) : <option>No rate cards found.</option>}
+                        </select>
+                        <button name='update-ratecard' onClick={this.updateRateCard}>Save</button>
+                    </TabPanel>
+                </Tabs>
             </div>
         );
     }
@@ -368,6 +510,26 @@ const loadRateCards = (token, storecode) => {
             })
     })
 }
+
+const calculateTotalHours = (hours, weightings) => {
+    let totalHours = 0;
+    for (let key of Object.keys(hours)) {
+        if (weightings[key]) {
+            totalHours += parseFloat(hours[key]) * parseFloat(weightings[key]) / 100;
+        } else {
+            totalHours += parseFloat(hours[key]);
+        }
+    }
+    return totalHours;
+}
+
+// const getTargets = (token, storecode) => {
+
+// }
+
+// const getWeightings = (token, storecode) => {
+
+// }
 
 export default compose(
     withAuthorisation(condition),
